bca8925535c432295e12f877b36d26a21756c2ac
warrenniu <warrenniu@gmail.com>
2020-10-15 13:47:55 -0400

    Completed

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,20 @@
+class CreateArtists < ActiveRecord::Migration[4.2]
+    def up
+    end
+  
+    def down
+    end
+
+    def change
+        create_table :artists do |t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown
+            
+        end
+    end
+
+
+      
+  end 
